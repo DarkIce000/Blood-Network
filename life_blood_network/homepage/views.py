e40b7fc0a087ef47804ca6347b15770b7e2bee5e
Aarav Kumar <DarkIce000>
2024-03-05 22:15:35 +0530

    05032024

--- life_blood_network/homepage/views.py
+++ life_blood_network/homepage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+
+def index(request):
+    return render(request, "homepage/index.html", {
+        "message": "Homepage"
+    })
